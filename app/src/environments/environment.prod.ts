0526bcf90ae632b115417c26617b1c712b9d9984
Yamil Lanz <ylanz@2050net.local>
2021-05-26 08:35:25 -0400

    apuntando al 10.10.0.21 ahora si

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -6,9 +6,9 @@ export const environment = {
 	apiUrl: 'http://10.10.0.16/backend/public/index.php/api/',
 	dirImgsSubidas: 'http://10.10.0.16/backend/public/subidos/',
 	pyApiUrl: 'http://10.10.0.16:5005/api/',
-	solpedURL: 'http://10.10.0.3:3005/api/',
+	solpedURL: 'http://10.10.0.21:3005/api/',
 	recepcionProductoUrl: 'http://10.10.0.3:3003/api/',
-	AdminAlmacenesUrl: 'http://10.10.0.3:3004/api/'
+	AdminAlmacenesUrl: 'http://10.10.0.12:3004/api/'
 
 	//TEST Christian
 	// apiUrl: 'http://10.10.0.16/backend/public/index.php/api/',
