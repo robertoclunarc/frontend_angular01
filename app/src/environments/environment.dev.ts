7daa0acfc927acf67f04d727e996529bd1c6c560
Christian Mejia <sysadm2.acero@gmail.com>
2021-06-11 13:08:23 -0400

    testing CI pipeline

--- app/src/environments/environment.dev.ts
+++ app/src/environments/environment.dev.ts
@@ -2,9 +2,10 @@
 export const environment = {
 	production: true,
 	name: 'desarrollo',
-	apiUrl: 'http://10.10.0.7:82/api/',
-	dirImgsSubidas: 'http://10.10.0.7:81/',
-	pyApiUrl: 'http://10.10.0.7:5005/api/',
+	apiUrl: 'http://10.10.0.16/backend/public/index.php/api/',
+	dirImgsSubidas: 'http://10.10.0.16/backend/public/subidos/',
+//	pyApiUrl: 'http://10.10.0.16:5005/api/',
 	solpedURL: 'http://10.10.0.7:3005/api/',
-	recepcionProductoUrl: 'http://10.10.0.7:3003/api/'
+	recepcionProductoUrl: 'http://10.10.0.7:3003/api/',
+	AdminAlmacenesUrl: 'http://10.10.0.7:3004/api/'
 };
--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -10,11 +10,4 @@ export const environment = {
 	recepcionProductoUrl: 'http://10.10.0.3:3003/api/',
 	AdminAlmacenesUrl: 'http://10.10.0.21:3004/api/'
 
-	//TEST Christian
-	// apiUrl: 'http://10.10.0.16/backend/public/index.php/api/',
-	// dirImgsSubidas: 'http://10.10.0.16/backend/public/subidos/',
-	// pyApiUrl: 'http://10.10.0.16:5005/api/',
-	// solpedURL: 'http://10.10.0.21:8002/api/',
-	// recepcionProductoUrl: 'http://10.10.0.3:3003/api/',
-	// AdminAlmacenesUrl: 'http://10.10.0.21:8001/api/'
 };
