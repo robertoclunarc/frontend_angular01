702f9bc6d27e9660a890aa1de2a56f8317c85b3a
Roberto Lunar <robertoclunarg@gmail.com>
2021-06-25 09:39:01 -0400

    incluir url en enviroment.dev

--- app/src/environments/environment.transven.ts
+++ app/src/environments/environment.transven.ts
@@ -7,5 +7,10 @@ export const environment = {
 	dirImgsSubidas: 'http://45.181.250.100:81/',
 	pyApiUrl: 'http://45.181.250.100:5005/api/',
 	solpedURL: 'http://45.181.250.100:3005/api/',
-	recepcionProductoUrl: 'http://45.181.250.100:3003/api/'
+	recepcionProductoUrl: 'http://45.181.250.100:3003/api/',
+
+	admCatalogoUrl: 'http://10.10.0.7:3006/api/adm/',
+	configUrl: 'http://10.10.0.7:3006/api/config/',
+	generalesUrl: 'http://10.10.0.7:3006/api/generales/',
+	comprasUrl: 'http://10.10.0.7:3006/api/compras/',
 };
