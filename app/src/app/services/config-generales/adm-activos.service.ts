d55f0f6506fcbcacee6a1f903d082f3c84c79edf
Yamil Lanz <ylanz@2050net.local>
2021-06-30 12:07:05 -0400

    se cambio el servicio activos para apuntar por ahora al php...nolver al back de config

--- app/src/app/services/config-generales/adm-activos.service.ts
+++ app/src/app/services/config-generales/adm-activos.service.ts
@@ -1,196 +1,215 @@
+import { Activo } from './../../models/activo';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
-import { Iadm_activos, Iconfig_activos_areas_negocios, Iconfig_activos_gerencias, IactivosJoin} from '../../models/config-generales/Iadm-activos';
+import { Iadm_activos, Iconfig_activos_areas_negocios, Iconfig_activos_gerencias, IactivosJoin } from '../../models/config-generales/Iadm-activos';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AdmActivosService {
-  private url: string;
-  admActivo: Iadm_activos={nombre:"", descripcion:"", serial:""};
-  activos: Iadm_activos[];
-  activosJoins: IactivosJoin[];
-  activoJoin: IactivosJoin;
-  constructor(private http: HttpClient) { 
-    this.url = environment.admCatalogoUrl + 'activos';
-  }
+	private url: string;
+	admActivo: Iadm_activos = { nombre: "", descripcion: "", serial: "" };
+	activos: Iadm_activos[];
+	activosJoins: IactivosJoin[];
+	activoJoin: IactivosJoin;
+	constructor(private http: HttpClient) {
+		this.url = environment.admCatalogoUrl + 'activos';
+	}
 
 
-  consultarTodos(): Observable<Iadm_activos[]> {
-    
-    return this.http.get<Iadm_activos[]>(this.url + '/consultar')
-      .pipe(
-        tap(result => this.log(`fetched Activos`)),
-        catchError(this.handleError('consultarTodos', []))
-      );
-  }
+	consultarTodos(): Observable<Iadm_activos[]> {
 
-  consultarJoin(): Observable<IactivosJoin[]> {    
-    return this.http.get<IactivosJoin[]>(this.url + '/consultarjoins')
-      .pipe(
-        tap(result => this.log(`fetched Activos Join`)),
-        catchError(this.handleError('consultarJoin', []))
-      );
-  }
+		return this.http.get<Iadm_activos[]>(this.url + '/consultar')
+			.pipe(
+				tap(result => this.log(`fetched Activos`)),
+				catchError(this.handleError('consultarTodos', []))
+			);
+	}
 
-  consultarActivosGcia(idAdmAct: number): Observable<Iconfig_activos_gerencias[]> {
-    const url = `${this.url}/activosporgerecias/${idAdmAct}`;
-    return this.http.get<Iconfig_activos_gerencias[]>(url)    
-      .pipe(
-        tap(result => {}),
-        catchError(this.handleError('consultarActivosGcia', []))
-      );
-  }
+	consultarJoin(): Observable<IactivosJoin[]> {
+		return this.http.get<IactivosJoin[]>(this.url + '/consultarjoins')
+			.pipe(
+				tap(result => this.log(`fetched Activos Join`)),
+				catchError(this.handleError('consultarJoin', []))
+			);
+	}
 
-   consultarActivosAreasNeg(idAdmAct: number):  Observable<Iconfig_activos_areas_negocios[]> {
-    const url = `${this.url}/activosporareas/${idAdmAct}`;
-    return this.http.get<Iconfig_activos_areas_negocios[]>(url)
-      .pipe(
-        tap(result => this.log(`fetched Activos por Gcias`)),
-        catchError(this.handleError('consultarActivosGcia', []))
-      );
-  }
+	consultarActivosGcia(idAdmAct: number): Observable<Iconfig_activos_gerencias[]> {
+		const url = `${this.url}/activosporgerecias/${idAdmAct}`;
+		return this.http.get<Iconfig_activos_gerencias[]>(url)
+			.pipe(
+				tap(result => { }),
+				catchError(this.handleError('consultarActivosGcia', []))
+			);
+	}
 
-
-  consultarPorId(_idAdmActivo: number): Observable<Iadm_activos[]> {
-    const activo: Iadm_activos = {
-      idAdmActivo: _idAdmActivo, 
-      nombre:null,
-      descripcion: null,
-      serial: null,
-      idAdmProducto: null,
-      idComprasEmpresa: null
-    }
-
-    return this.viewFromAnyField(activo);
-  }
-
-  viewFromAnyField(activo: Iadm_activos): Observable<Iadm_activos[]> {
-    let _id: any;
-    let _nombre = activo.nombre;
-    let _desc = activo.descripcion;
-    let _serial = activo.serial;
-    let _idProducto: any;
-    let _idComEmpresa: any
-    
-    if (activo.idAdmActivo==null){
-      _id="NULL";
-    }else {
-      _id=activo.idAdmActivo.toString();
-    }
-    if (activo.nombre==null){
-      _nombre="NULL";
-    }
-    if (activo.descripcion==null){
-      _desc="NULL";
-    }
-    if (activo.serial==null){
-      _serial="NULL";
-    }
-    if (activo.idAdmProducto==null){
-      _idProducto="NULL";
-    }else{
-      _idProducto= activo.idAdmProducto.toString();
-    }
-
-    if (activo.idComprasEmpresa==null){
-      _idComEmpresa="NULL";
-    }else{
-      _idComEmpresa=activo.idComprasEmpresa.toString();
-    }
-
-    const url = `${this.url}/filtar/${_id}/${_nombre}/${_desc}/${_serial}/${_idProducto}/${_idComEmpresa}`;
-
-    return this.http.get<Iadm_activos[]>(url)
-      .pipe(
-        tap(result => this.log(`fetched Activo`)),
-        catchError(this.handleError('viewFromAnyField', []))
-      );
-  }
-
-  getPorGerencias(idGerencia: number): Promise<Iadm_activos[]> {
-    return this.consultarTodos().toPromise();
-  }
+	consultarActivosAreasNeg(idAdmAct: number): Observable<Iconfig_activos_areas_negocios[]> {
+		const url = `${this.url}/activosporareas/${idAdmAct}`;
+		return this.http.get<Iconfig_activos_areas_negocios[]>(url)
+			.pipe(
+				tap(result => this.log(`fetched Activos por Gcias`)),
+				catchError(this.handleError('consultarActivosGcia', []))
+			);
+	}
 
 
-  registrar(activo: Iadm_activos) {
-    return this.http.post<Iadm_activos>(this.url + '/insertar', activo).pipe(
-      tap(result => {this.admActivo=result; this.log(`Activo insertado`)}),
-      catchError(this.handleError('registrar activo', []))
-    );
-  }
+	consultarPorId(_idAdmActivo: number): Observable<Iadm_activos[]> {
+		const activo: Iadm_activos = {
+			idAdmActivo: _idAdmActivo,
+			nombre: null,
+			descripcion: null,
+			serial: null,
+			idAdmProducto: null,
+			idComprasEmpresa: null
+		}
 
-  insertarareanegocio(areaNegocio: Iconfig_activos_areas_negocios){
-    return this.http.post(this.url + '/insertarareanegocio', areaNegocio).pipe(
-      tap(result => {
-      }),
-      catchError(this.handleError('registrar relacion activo-area negocio', []))
-    );
+		return this.viewFromAnyField(activo);
+	}
 
-  }
+	viewFromAnyField(activo: Iadm_activos): Observable<Iadm_activos[]> {
+		let _id: any;
+		let _nombre = activo.nombre;
+		let _desc = activo.descripcion;
+		let _serial = activo.serial;
+		let _idProducto: any;
+		let _idComEmpresa: any
 
-  insertaractivogerencia(activoGcia: Iconfig_activos_gerencias){
-    return this.http.post(this.url + '/insertaractivogerencia', activoGcia).pipe(
-      tap(result => {
-      }),
-      catchError(this.handleError('registrar relacion activo-gerencia', []))
-    );
+		if (activo.idAdmActivo == null) {
+			_id = "NULL";
+		} else {
+			_id = activo.idAdmActivo.toString();
+		}
+		if (activo.nombre == null) {
+			_nombre = "NULL";
+		}
+		if (activo.descripcion == null) {
+			_desc = "NULL";
+		}
+		if (activo.serial == null) {
+			_serial = "NULL";
+		}
+		if (activo.idAdmProducto == null) {
+			_idProducto = "NULL";
+		} else {
+			_idProducto = activo.idAdmProducto.toString();
+		}
 
-  }
+		if (activo.idComprasEmpresa == null) {
+			_idComEmpresa = "NULL";
+		} else {
+			_idComEmpresa = activo.idComprasEmpresa.toString();
+		}
 
-  actualizar(activoActual: Iadm_activos) {    
-    const url = `${this.url}/actualizar/${activoActual.idAdmActivo}`;
+		const url = `${this.url}/filtar/${_id}/${_nombre}/${_desc}/${_serial}/${_idProducto}/${_idComEmpresa}`;
 
-    return this.http.put(url, activoActual).pipe(
-      tap(result => {
-      }),
-      catchError(this.handleError('actualizando Activo', []))
-    );
-  }
+		return this.http.get<Iadm_activos[]>(url)
+			.pipe(
+				tap(result => this.log(`fetched Activo`)),
+				catchError(this.handleError('viewFromAnyField', []))
+			);
+	}
 
-  eliminarActivosGcias(idAdmActivo: number) {    
-    const url = `${this.url}/eliminargerencias/${idAdmActivo}`;
-    
-    return this.http.delete(url).pipe(
-      tap(result => {
-      }),
-      catchError(this.handleError('error eliminando gerencias', []))
-    );
-  }
+	// getPorGerencias(idGerencia: number): Promise<Iadm_activos[]> {
+	//   // return this.consultarTodos().toPromise();
+	//   //Esto hacerlo solo cuando este listo
+	//   return this.http.post<Iadm_activos>(this.url + '/insertar', activo).pipe(
+	//     tap(result => {this.admActivo=result; this.log(`Activo insertado`)}),
+	//     catchError(this.handleError('registrar activo', []))
+	//   );
+	// }
 
-  eliminarActivosAreaNegocio(idAdmActivo: number) {    
-    const url = `${this.url}/eliminarareasnegocio/${idAdmActivo}`;
-    
-    return this.http.delete(url).pipe(
-      tap(result => {
-      }),
-      catchError(this.handleError('error eliminando areas negocio', []))
-    );
-  }
+	getPorGerencias(idGerencia: number) {
 
-  eliminar(idAdmActivo: number){
-    const url = `${this.url}/eliminar/${idAdmActivo}`;
-    
-    return this.http.delete(url).pipe(
-      tap(result => {
-      }),
-      catchError(this.handleError('error eliminando Activo', []))
-    );
-  }
+		return this.http.get<Activo[]>(environment.apiUrl + `activos/gerencia/` + `${idGerencia}`);
+		// return this.consultarTodos().toPromise();
+		//Esto hacerlo solo cuando este listo
+		// return this.http.post<Iadm_activos>(this.url + '/insertar', activo).pipe(
+		//   tap(result => {this.admActivo=result; this.log(`Activo insertado`)}),
+		//   catchError(this.handleError('registrar activo', []))
+		// );
+	}
+
+	//
 
 
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(error);
-      return of(result as T);
-    };
-  }
+	registrar(activo: Iadm_activos) {
+		return this.http.post<Iadm_activos>(this.url + '/insertar', activo).pipe(
+			tap(result => { this.admActivo = result; this.log(`Activo insertado`) }),
+			catchError(this.handleError('registrar activo', []))
+		);
+	}
 
-  private log(message: string) {
-    console.log('UserService: ' + message);
-  }
+	insertarareanegocio(areaNegocio: Iconfig_activos_areas_negocios) {
+		return this.http.post(this.url + '/insertarareanegocio', areaNegocio).pipe(
+			tap(result => {
+			}),
+			catchError(this.handleError('registrar relacion activo-area negocio', []))
+		);
+
+	}
+
+	insertaractivogerencia(activoGcia: Iconfig_activos_gerencias) {
+		return this.http.post(this.url + '/insertaractivogerencia', activoGcia).pipe(
+			tap(result => {
+			}),
+			catchError(this.handleError('registrar relacion activo-gerencia', []))
+		);
+
+	}
+
+	actualizar(activoActual: Iadm_activos) {
+		const url = `${this.url}/actualizar/${activoActual.idAdmActivo}`;
+
+		return this.http.put(url, activoActual).pipe(
+			tap(result => {
+			}),
+			catchError(this.handleError('actualizando Activo', []))
+		);
+	}
+
+	eliminarActivosGcias(idAdmActivo: number) {
+		const url = `${this.url}/eliminargerencias/${idAdmActivo}`;
+
+		return this.http.delete(url).pipe(
+			tap(result => {
+			}),
+			catchError(this.handleError('error eliminando gerencias', []))
+		);
+	}
+
+	eliminarActivosAreaNegocio(idAdmActivo: number) {
+		const url = `${this.url}/eliminarareasnegocio/${idAdmActivo}`;
+
+		return this.http.delete(url).pipe(
+			tap(result => {
+			}),
+			catchError(this.handleError('error eliminando areas negocio', []))
+		);
+	}
+
+	eliminar(idAdmActivo: number) {
+		const url = `${this.url}/eliminar/${idAdmActivo}`;
+
+		return this.http.delete(url).pipe(
+			tap(result => {
+			}),
+			catchError(this.handleError('error eliminando Activo', []))
+		);
+	}
+
+
+	private handleError<T>(operation = 'operation', result?: T) {
+		return (error: any): Observable<T> => {
+			console.error(error);
+			return of(result as T);
+		};
+	}
+
+	private log(message: string) {
+		console.log('UserService: ' + message);
+	}
 
 }
