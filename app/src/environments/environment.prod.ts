e6ee43aad87a36b547a132b4fafac852e1c58a78
Yamil Lanz <ylanz@2050net.local>
2021-06-30 11:31:16 -0400

    cambio al 21

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -10,9 +10,9 @@ export const environment = {
 	recepcionProductoUrl: 'http://10.10.0.3:3003/api/',
 	AdminAlmacenesUrl: 'http://10.10.0.21:3004/api/',
 
-	admCatalogoUrl: 'http://10.10.0.7:3006/api/adm/',
-	configUrl: 'http://10.10.0.7:3006/api/config/',
-	generalesUrl: 'http://10.10.0.7:3006/api/generales/',
-	comprasUrl: 'http://10.10.0.7:3006/api/compras/',
+	admCatalogoUrl: 'http://10.10.0.21:3006/api/adm/',
+	configUrl: 'http://10.10.0.21:3006/api/config/',
+	generalesUrl: 'http://10.10.0.21:3006/api/generales/',
+	comprasUrl: 'http://10.10.0.21:3006/api/compras/',
 
 };
