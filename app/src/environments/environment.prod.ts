2d0d7290f27d580cd541ff0b4f392dfb09474db4
Roberto Lunar <desarrollo2.acero@gmail.com>
2021-08-24 09:32:53 -0400

    update in enviroments

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
 	dirImgsSubidas: 'http://10.10.0.16/backend/public/subidos/',
 	pyApiUrl: 'http://10.10.0.16:5005/api/',
 	solpedURL: 'http://10.10.0.21:3005/api/',
-	recepcionProductoUrl: 'http://10.10.0.3:3003/api/inventario',
+	recepcionProductoUrl: 'http://10.10.0.3:3003/api/inventario/',
 	AdminAlmacenesUrl: 'http://10.10.0.21:3004/api/',
 
 	admCatalogoUrl: 'http://10.10.0.21:3006/api/adm/',
